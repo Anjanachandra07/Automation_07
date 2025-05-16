9025edc4c74a42779131aef26a2d521ec4a87701
Anjanachandra07 <anjana19193@gmail.com>
2025-05-16 12:26:02 +0530

    initial commit

---
+++ JavaBasics/src/day1/Solution.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class Solution {
+	
+	public int getBeats() {
+		return 0;
+		
+	}
+	
+	public void setBeats(int beets)
+	{
+		int beats=beets;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution ob = new Solution();
+		ob.setBeats(21);
+		System.out.println(ob.getBeats());
+
+	}
+
+}
