22a368ed6ac146dd87f3cc7fd00f2f7aee660090
Anjanachandra07 <anjana19193@gmail.com>
2025-05-26 00:38:01 +0530

    Added Polymorphism

---
+++ JavaBasics_Tutorial/src/day5/Polymorphism.java
@@ -0,0 +1,20 @@
+package day5;
+
+public class Polymorphism {
+
+			public static void main(String[] args) {
+				Student s1 = new Student();
+				s1.lunchBreak();
+				
+				Staff st1 = new Staff();
+				st1.lunchBreak();
+				
+				Teacher t1 = new Teacher();
+				t1.lunchBreak();
+				
+
+			}
+
+
+	}
+
